550bf9d7dabf0f1eb614d47b6e3a02f7e0660516
taufikfaturrahman <taufikfaturrahman12@gmail.com>
2025-02-02 23:17:42 +0700

    menyala

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ window.addEventListener("scroll", () => {
 
   navbarHome.classList.toggle("scroll", post);
 });
+
+// menyala
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("mouseenter", function () {
+    setTimeout(() => this.classList.add("active"), 300); // Delay 300ms sebelum menyala
+  });
+
+  link.addEventListener("mouseleave", function () {
+    setTimeout(() => this.classList.remove("active"), 500); // Tetap menyala 500ms setelah keluar
+  });
+});
